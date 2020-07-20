a0d440884baa7900c1f0341c01d690256ca6a5a2
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-20 19:25:34 +0100

    Update MainActivity.java

--- app/src/main/java/com/denzo/wakil/MainActivity.java
+++ app/src/main/java/com/denzo/wakil/MainActivity.java
@@ -3,6 +3,10 @@ package com.denzo.wakil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_one, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mail:
+                // do something
+                return true;
+            case R.id.share:
+                // do something
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
