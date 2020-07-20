c29159e0069a99acb9915620cc9bf747f1e01c20
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-20 19:51:25 +0100

    Update AboutRecyclerviewAdapter.java

--- app/src/main/java/com/denzo/wakil/adapters/AboutRecyclerviewAdapter.java
+++ app/src/main/java/com/denzo/wakil/adapters/AboutRecyclerviewAdapter.java
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.denzo.wakil.Models.Member;
 import com.denzo.wakil.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
