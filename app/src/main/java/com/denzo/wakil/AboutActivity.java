30db12f1875fa09c74931fcbdd4cb639ac0fdeac
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-20 19:33:16 +0100

    Update AboutActivity.java

--- app/src/main/java/com/denzo/wakil/AboutActivity.java
+++ app/src/main/java/com/denzo/wakil/AboutActivity.java
@@ -1,14 +1,48 @@
 package com.denzo.wakil;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class AboutActivity extends AppCompatActivity {
+    public static String jsonUrl;
 
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    LinearLayoutManager manager;
+    SwipeRefreshLayout swipeRefreshLayout;
+    ImageView imageViewOfficeLogo;
+    ImageView imageViewGooglePlay, imageViewFacebook, imageViewGroup, imageViewYoutube, imageViewGithub, imageViewWeb;
+    LinearLayout linearLayoutParent;
+
+    ArrayList<Member> members = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        swipeRefreshLayout = findViewById(R.id.srl);
+        linearLayoutParent = findViewById(R.id.parentLL);
+        imageViewOfficeLogo = findViewById(R.id.officeLogoImage);
+
+        imageViewGooglePlay = findViewById(R.id.googlePlayLogo);
+        imageViewFacebook = findViewById(R.id.facebookLogo);
+        imageViewGroup = findViewById(R.id.groupLogo);
+        imageViewYoutube = findViewById(R.id.youtubeLogo);
+        imageViewGithub = findViewById(R.id.githubLogo);
+        imageViewWeb = findViewById(R.id.webLogo);
+
+        recyclerView = findViewById(R.id.officeAboutRecyclerView);
+        manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        adapter = new AboutRecyclerviewAdapter(AboutActivity.this, members);
+        recyclerView.setAdapter(adapter);
     }
 }
