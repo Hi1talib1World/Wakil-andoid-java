3410adbaf46ea69f4cd81fca41f6a55e26c21575
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-08-11 22:12:23 +0100

    Update DummyFragment.java

--- app/src/main/java/com/denzo/wakil/DummyFragment.java
+++ app/src/main/java/com/denzo/wakil/DummyFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public static class DummyFragment extends Fragment {
+public class DummyFragment extends Fragment {
     int color;
 
     public DummyFragment() {
