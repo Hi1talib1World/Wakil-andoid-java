13d4071a4eca1ab408f1e0e6d23ce97faf3ea2fb
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-08-12 12:42:17 +0100

    Update Product_List.java

--- app/src/main/java/com/denzo/wakil/addFavorites/Product_List.java
+++ app/src/main/java/com/denzo/wakil/addFavorites/Product_List.java
@@ -1,4 +1,25 @@
 package com.denzo.wakil.addFavorites;
 
 public class Product_List {
+    private int id;
+    private String name;
+    private String image;
+
+    public Product_List(int id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
