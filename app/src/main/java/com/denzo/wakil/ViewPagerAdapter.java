20e7d1e4cc3fb9b763b2170168d276f6f4c8a6e8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-01-04 22:39:05 +0100

    Update ViewPagerAdapter.java

--- app/src/main/java/com/denzo/wakil/ViewPagerAdapter.java
+++ app/src/main/java/com/denzo/wakil/ViewPagerAdapter.java
@@ -4,11 +4,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.ListFragment;
-
-
 import java.util.List;
-
-
 
 private static class ViewPagerAdapter extends FragmentPagerAdapter {
     ListFragment mFragmentList;
