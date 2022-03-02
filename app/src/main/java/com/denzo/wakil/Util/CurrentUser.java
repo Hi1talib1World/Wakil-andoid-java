cf6802981edbdb9d7c667422813d8dc7c1cde4f7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-03-02 20:47:25 +0100

    Create CurrentUser.java

---
+++ app/src/main/java/com/denzo/wakil/Util/CurrentUser.java
@@ -0,0 +1,9 @@
+package com.denzo.wakil.Util;
+
+public class CurrentUser {
+    public static String username;
+    public CurrentUser(){}
+    public CurrentUser(String username){
+        this.username = username;
+    }
+}
