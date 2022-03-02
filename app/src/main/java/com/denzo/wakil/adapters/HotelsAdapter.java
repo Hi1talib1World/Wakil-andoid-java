c0e91f5a2ac0e962c08d88b81e839298798080e1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-03-02 22:54:23 +0100

    Update HotelsAdapter.java

--- app/src/main/java/com/denzo/wakil/adapters/HotelsAdapter.java
+++ app/src/main/java/com/denzo/wakil/adapters/HotelsAdapter.java
@@ -11,7 +11,9 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.denzo.wakil.R;
+import com.denzo.wakil.Util.HotelView;
 
 import java.util.List;
 
