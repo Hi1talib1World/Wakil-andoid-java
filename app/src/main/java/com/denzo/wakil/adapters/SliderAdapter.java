4b0a2dfe5c4129ceda9caaaf53a137ecb337cacf
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-03-03 07:57:55 +0100

    Update SliderAdapter.java

--- app/src/main/java/com/denzo/wakil/adapters/SliderAdapter.java
+++ app/src/main/java/com/denzo/wakil/adapters/SliderAdapter.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.example.myhotels.R;
+import com.denzo.wakil.R;
 import com.smarteist.autoimageslider.SliderViewAdapter;
 
 public class SliderAdapter extends SliderViewAdapter<SliderAdapter.SliderAdapterVH> {
