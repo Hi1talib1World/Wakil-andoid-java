a6a59e82d121d74effd0c4676188f96a576bc7a6
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-13 20:09:18 +0100

    Update MainActivity.java

--- app/src/main/java/com/denzo/wakil/MainActivity.java
+++ app/src/main/java/com/denzo/wakil/MainActivity.java
@@ -11,13 +11,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.denzo.wakil.Decoration.GridSpacingItemDecoration;
 import com.denzo.wakil.Util.Bookings;
