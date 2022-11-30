841330071da19c77c82430a5bee7bc820448aba7
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-12-01 00:21:45 +0100

    Create LoginActivity.java

---
+++ app/src/main/java/com/denzo/wakil/Login/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.denzo.wakil.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.denzo.wakil.MainActivity;
+import com.denzo.wakil.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
