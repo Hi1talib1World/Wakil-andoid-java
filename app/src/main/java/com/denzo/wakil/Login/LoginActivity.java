242b7efe5145e4a94cec898ee779ce98b5bcb5fa
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-12-04 01:17:13 +0100

    Update LoginActivity.java

--- app/src/main/java/com/denzo/wakil/Login/LoginActivity.java
+++ app/src/main/java/com/denzo/wakil/Login/LoginActivity.java
@@ -17,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_acitvity);
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         login = findViewById(R.id.login);
         login.setOnClickListener(view -> {
