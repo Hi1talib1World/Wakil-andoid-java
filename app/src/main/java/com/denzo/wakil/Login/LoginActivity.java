50ef39ad12c9b0898aa954a66472635c8a2a1b49
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-12-05 13:41:43 +0100

    Update LoginActivity.java

--- app/src/main/java/com/denzo/wakil/Login/LoginActivity.java
+++ app/src/main/java/com/denzo/wakil/Login/LoginActivity.java
@@ -1,11 +1,11 @@
 package com.denzo.wakil.Login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.denzo.wakil.MainActivity;
 import com.denzo.wakil.R;
@@ -17,8 +17,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_acitvity);
-        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         login = findViewById(R.id.login);
         login.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
