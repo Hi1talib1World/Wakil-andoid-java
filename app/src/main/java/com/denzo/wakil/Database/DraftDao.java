16b417768a42e96e994e9226989c51d5df604321
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-03 23:26:07 +0100

    feat: update DraftDao logic

--- app/src/main/java/com/denzo/wakil/Database/DraftDao.java
+++ app/src/main/java/com/denzo/wakil/Database/DraftDao.java
@@ -21,4 +21,7 @@ public interface DraftDao {
 
     @Delete
     void deleteDraft(DraftEntity draft);
+
+    @Query("DELETE FROM drafts WHERE username = :username")
+    void clearAllDrafts(String username);
 }
