cdf290469fc93e07a35bc35e1054d6595d6c5a09
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-03 23:36:43 +0100

    feat: update MainActivity logic

--- app/src/main/java/com/denzo/wakil/MainActivity.java
+++ app/src/main/java/com/denzo/wakil/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
