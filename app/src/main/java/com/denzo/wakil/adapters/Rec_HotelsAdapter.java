59355960f7ef0d25eb96f9cd7be75a5bbe3b8341
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-05 16:17:28 +0100

    feat: update Rec_HotelsAdapter logic

--- app/src/main/java/com/denzo/wakil/adapters/Rec_HotelsAdapter.java
+++ app/src/main/java/com/denzo/wakil/adapters/Rec_HotelsAdapter.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.denzo.wakil.HotelViewer;
+import com.denzo.wakil.ui.booking.HotelViewer;
 import com.denzo.wakil.R;
 import com.denzo.wakil.Util.HotelView;
 
@@ -26,15 +26,15 @@ public class Rec_HotelsAdapter extends RecyclerView.Adapter<Rec_HotelsAdapter.My
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView rec_title, rec_location,rec_rating,rec_features;
         public ImageView rec_thumbnail;
-        public Button rec_view;
+        public View rec_card;
         public MyViewHolder(View view) {
             super(view);
+            rec_card = view.findViewById(R.id.rec_card_view);
             rec_thumbnail = view.findViewById(R.id.rec_thumbnail);
             rec_title =  view.findViewById(R.id.rec_title);
             rec_location =  view.findViewById(R.id.rec_location);
             rec_rating =  view.findViewById(R.id.rec_rating);
             rec_features = view.findViewById(R.id.rec_features);
-            rec_view = view.findViewById(R.id.rec_viewbutton);
         }
     }
 
@@ -60,11 +60,11 @@ public class Rec_HotelsAdapter extends RecyclerView.Adapter<Rec_HotelsAdapter.My
         holder.rec_features.setText(hotel.getFeatures());
         holder.rec_rating.setText(String.valueOf(hotel.getRating()));
         Glide.with(rec_Context).load(hotel.getThumbnail()).into(holder.rec_thumbnail);
-        holder.rec_view.setOnClickListener(new View.OnClickListener() {
+        holder.rec_card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(rec_Context, HotelViewer.class);
-                intent.putExtra("hotelname",holder.rec_title.getText().toString());
+                intent.putExtra("hotelname", hotel.getName());
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 rec_Context.startActivity(intent);
             }
