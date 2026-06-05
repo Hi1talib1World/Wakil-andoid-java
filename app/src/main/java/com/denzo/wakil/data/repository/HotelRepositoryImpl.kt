0dcd0f7270587ddab90d13ebca1aaf13adf03076
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-05 16:17:29 +0100

    feat: update HotelRepositoryImpl logic

--- app/src/main/java/com/denzo/wakil/data/repository/HotelRepositoryImpl.kt
+++ app/src/main/java/com/denzo/wakil/data/repository/HotelRepositoryImpl.kt
@@ -55,7 +55,7 @@ class HotelRepositoryImpl @Inject constructor(
         return blockedDao.getBlockedUsers(username)
     }
 
-    override fun getBlockedPosts(username: String): List<Integer> {
+    override fun getBlockedPosts(username: String): List<Int> {
         return blockedDao.getBlockedPosts(username)
     }
 }
